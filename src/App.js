d7c0c1acd402f3f8fe154ad2abbb6399697e2656
urinaner <urinaner@naver.com>
2022-12-17 14:18:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './componets/Customer';
+
+const customers = [{
+  'id': 1,
+  'image': 'http://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '942910',
+  'gender': '남자',
+  'job': '대학생'
+
+},
+{
+  'id': 2,
+  'image': 'http://placeimg.com/64/64/2',
+  'name': '장영재',
+  'birthday': '991027',
+  'gender': '남자',
+  'job': '대학생'
+
+},
+{
+  'id': 3,
+  'image': 'http://placeimg.com/64/64/3',
+  'name': '장다연',
+  'birthday': '040707',
+  'gender': '여자',
+  'job': '학생'
+
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop</h2>
-      
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
